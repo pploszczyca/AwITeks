87b6b9bc9620c054694ec239330e5ab1a5b02ad2
dmncp <damiancyper@gmail.com>
2022-04-20 15:59:08 +0200

    styles moved to new SiteTitleAndIconsStyle.tsx file

--- frontend/src/components/SiteTitleAndIcons/SiteTitleAndIcons.tsx
+++ frontend/src/components/SiteTitleAndIcons/SiteTitleAndIcons.tsx
@@ -1,30 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBell, faCircleUser as faUser } from '@fortawesome/free-solid-svg-icons'
-import styled from "styled-components";
+import {FixedContainer, PageTitle} from "./SiteTitleAndIconsStyle";
 
-const PageTitle = styled.h1`
-  color: #0FC2C0;
-`;
-
-const FixedContainer = styled.div`
-  display: flex;
-  align-items: center;
-  column-gap: 15px;
-
-  position: relative;
-  left: var(--sidebar-width);
-
-  width: var(--content-width);
-  margin: var(--content-margin-y) var(--content-margin-x);
-  
-  & .icons{
-    position: absolute;
-    left: 100%;
-    transform: translateX(-100%);
-    padding-right: var(--content-margin-x);
-  }
-`
 
 function SiteTitleAndIcons() {
     return (
---
+++ frontend/src/components/SiteTitleAndIcons/SiteTitleAndIconsStyle.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const PageTitle = styled.h1`
+  color: #0FC2C0;
+`;
+
+export const FixedContainer = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 15px;
+
+  position: relative;
+  left: var(--sidebar-width);
+
+  width: var(--content-width);
+  margin: var(--content-margin-y) var(--content-margin-x);
+  
+  & .icons{
+    position: absolute;
+    left: 100%;
+    transform: translateX(-100%);
+    padding-right: var(--content-margin-x);
+  }
+`
