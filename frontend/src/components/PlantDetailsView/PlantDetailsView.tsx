96f489122a8d7294dafe5f79f04b507fb61d48dd
ewier <e.wierciak@gmail.com>
2022-04-23 23:42:58 +0200

    fixed warning

--- frontend/src/components/PlantDetailsView/PlantDetailsView.tsx
+++ frontend/src/components/PlantDetailsView/PlantDetailsView.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Card, Col, Row } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 import { useNavigate, useParams } from 'react-router-dom'
 import { mockPlants } from '../../utils/mockData';
 import Calendar from '../Calendar/Calendar';
