c27ee7e86b4e46f71723bc0d91d9295bcbfe606a
ewier <e.wierciak@gmail.com>
2022-04-23 22:15:48 +0200

    added 'wymagania podstawowe' and buttons

--- frontend/src/components/PlantDetailsView/PlantDetailsView.tsx
+++ frontend/src/components/PlantDetailsView/PlantDetailsView.tsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react'
-import { Card, Col, Row } from 'react-bootstrap';
+import { Button, Card, Col, Row } from 'react-bootstrap';
 import { useNavigate, useParams } from 'react-router-dom'
 import { mockPlants } from '../../utils/mockData';
 import Calendar from '../Calendar/Calendar';
 import { DetailsWrapper, InfoWrapper, TitleSeparator } from './PlantDetailsViewStyles';
 import { ContentContainer } from "../App/AppStyle";
+
+
+function edit_plant(){}
+
+function add_note(){}
+
+function personal_requirements(){}
 
 const PlantDetailsView: React.FC<{}> = (props) => {
     const { plantId } = useParams();
@@ -31,16 +38,14 @@ const PlantDetailsView: React.FC<{}> = (props) => {
                                 <TitleSeparator />
 
                                 <Card.Text>
+                                    <span className="d-block">Nazwa rośliny: {plant.name}</span>
+                                    <span className="d-block">Gatunek: {plant.species.name}</span>
                                     <span className="d-block">Średnia długość życia gautnku: {plant.species.maxAge}</span>
-                                    <span className="d-block">Wymagane nasłonecznienie: {plant.species.neededInsolation}</span>
-                                    <span className="d-block">Częstotliwość podlewania: {plant.species.waterRoutine} / tydzień</span>
-                                    <span className="d-block">Zalecana ilość wody: {plant.species.waterDose}l</span>
-                                    <span className="d-block">Częstotliwość nawożenia: {plant.species.fertilizationRoutine} / miesiąc</span>
-                                    <span className="d-block">Intensywność nawożenia: {plant.species.fertilizationDose}</span>
                                 </Card.Text>
 
                             </Card.Body>
                         </Card>
+                        
                     </Col>
 
                     <Col xxl={6}>
@@ -53,6 +58,32 @@ const PlantDetailsView: React.FC<{}> = (props) => {
                                     <span className="d-block">Data ostatniego podlania: 24.01.2022</span>
                                     <span className="d-block">Data ostatnieno nawożenia: 26.12.2021</span>
                                 </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card as={DetailsWrapper}>
+                            <Card.Body>
+                                <Card.Title style={{ fontSize: 26 }}>Wymagania podstawowe</Card.Title>
+                                <TitleSeparator />
+
+                                <Card.Text>
+                                    <span className="d-block">Wymagane nasłonecznienie: {plant.species.neededInsolation}</span>
+                                    <span className="d-block">Częstotliwość podlewania: {plant.species.waterRoutine} / tydzień</span>
+                                    <span className="d-block">Zalecana ilość wody: {plant.species.waterDose}l</span>
+                                    <span className="d-block">Częstotliwość nawożenia: {plant.species.fertilizationRoutine} / miesiąc</span>
+                                    <span className="d-block">Intensywność nawożenia: {plant.species.fertilizationDose}</span>
+                                </Card.Text>
+                                <div style={{ flexDirection:"row"}}>
+                                        <Button onClick = {() => edit_plant()} style={{justifyContent:'space-between', marginRight: 10}}>
+                                            Edytuj roślinę
+                                        </Button>
+                                        <Button onClick = {() => add_note()} style={{justifyContent:'space-between', marginRight: 10}}>
+                                            Dodaj notatkę
+                                        </Button>
+                                        <Button onClick = {() => personal_requirements()} style={{justifyContent:'space-between', marginRight: 10}}>
+                                            Własne wymagania
+                                        </Button>
+                                </div>
+
                             </Card.Body>
                         </Card>
                     </Col>
