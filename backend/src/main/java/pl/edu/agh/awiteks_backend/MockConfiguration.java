a9c209bedb4d57cce2f45e1273f4b2e19ea5b407
kkoz34 <jkozlak@student.agh.edu.pl>
2022-04-28 14:42:09 +0200

    added activities to plants

--- backend/src/main/java/pl/edu/agh/awiteks_backend/MockConfiguration.java
+++ backend/src/main/java/pl/edu/agh/awiteks_backend/MockConfiguration.java
@@ -6,6 +6,7 @@ import pl.edu.agh.awiteks_backend.repositories.Repository;
 import pl.edu.agh.awiteks_backend.repositories.RepositoryImp;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @org.springframework.context.annotation.Configuration
@@ -76,17 +77,28 @@ public class MockConfiguration {
 
     @Bean
     public Repository<Plant> getPlantRepository(User user, Repository<Species> speciesRepository, Species species, String url) {
+        //  public Activity(int id, Plant plant, ActivityType activityType, Date date)
 
         //  public Plant(int id, String name, User user, Species spiece, String note, Insolation actualInsolation)
         Plant plant1 = new Plant(0, "My Kaktus", user, speciesRepository.get(1).get(), "Simple Kaktus note", Insolation.HIGH, url);
+        Activity activity1 = new Activity(0, plant1, ActivityType.FERTILISATION, new Date(12,12,2000));
+        Activity activity11 = new Activity(1, plant1, ActivityType.WATERING, new Date(12,12,2000));
 
         Plant plant2 = new Plant(1, "My Fiołek", user, speciesRepository.get(2).get(), "Simple Fiołek note", Insolation.LOW, url);
+        Activity activity2 = new Activity(2, plant2, ActivityType.FERTILISATION, new Date(12,12,2000));
+        Activity activity22 = new Activity(3, plant2, ActivityType.WATERING, new Date(12,12,2000));
 
         Plant plant3 = new Plant(2, "My Storczyk", user, speciesRepository.get(3).get(), "Simple Storczyk note", Insolation.HIGH, url);
+        Activity activity3 = new Activity(4, plant2, ActivityType.FERTILISATION, new Date(12,12,2000));
+        Activity activity33 = new Activity(5, plant2, ActivityType.WATERING, new Date(12,12,2000));
 
         Plant plant4 = new Plant(3, "My Bazylia", user, speciesRepository.get(4).get(), "Simple Bazylia note", Insolation.HIGH, "eh");
+        Activity activity4 = new Activity(6, plant2, ActivityType.FERTILISATION, new Date(12,12,2000));
+        Activity activity44 = new Activity(7, plant2, ActivityType.WATERING, new Date(12,12,2000));
 
         Plant plant5 = new Plant(4, "My Mięta", user, speciesRepository.get(5).get(), "Simple Mięta note", Insolation.HIGH, true, url);
+        Activity activity5 = new Activity(8, plant2, ActivityType.FERTILISATION, new Date(12,12,2000));
+        Activity activity55 = new Activity(9, plant2, ActivityType.WATERING, new Date(12,12,2000));
 
         List<AbstractModel<Plant>> plantList = new ArrayList<>();
         plantList.add(plant1);
@@ -94,6 +106,22 @@ public class MockConfiguration {
         plantList.add(plant3);
         plantList.add(plant4);
         plantList.add(plant5);
+
+        plant1.addActivity(activity1);
+        plant1.addActivity(activity11);
+
+        plant2.addActivity(activity2);
+        plant2.addActivity(activity22);
+
+        plant3.addActivity(activity3);
+        plant3.addActivity(activity33);
+
+        plant4.addActivity(activity4);
+        plant4.addActivity(activity44);
+
+        plant5.addActivity(activity5);
+        plant5.addActivity(activity55);
+
 
         user.addPlant(plant1);
         user.addPlant(plant2);
