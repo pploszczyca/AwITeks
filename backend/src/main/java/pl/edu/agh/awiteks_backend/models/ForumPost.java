cf4725d1616a6f8c9680102d0f4fcac7a8952f55
Marcin Malcher <malcher@student.agh.edu.pl>
2022-04-30 21:34:51 +0200

    Add reference to ForumThread inside ForumPost
    I forgor

--- backend/src/main/java/pl/edu/agh/awiteks_backend/models/ForumPost.java
+++ backend/src/main/java/pl/edu/agh/awiteks_backend/models/ForumPost.java
@@ -3,16 +3,19 @@ package pl.edu.agh.awiteks_backend.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@JsonIgnoreProperties({"user"})
+@JsonIgnoreProperties({"user", "thread"})
 public class ForumPost extends AbstractModel<ForumPost> {
     @Schema(required = true)
     private String content;
     @Schema(required = true)
     private User author;
+    @Schema(required = true)
+    private ForumThread thread;
 
-    public ForumPost(int id, User author, String content) {
+    public ForumPost(int id, User author, ForumThread thread, String content) {
         super(id, "");
         this.author = author;
+        this.thread = thread;
         this.content = content;
     }
 
@@ -21,12 +24,14 @@ public class ForumPost extends AbstractModel<ForumPost> {
 
     @Override
     public ForumPost copy() {
-        return new ForumPost(this.id, this.author, this.content);
+        return new ForumPost(this.id, this.author, this.thread, this.content);
     }
 
     public User getAuthor() {
         return author;
     }
+
+    public ForumThread getThread(){return thread;}
 
     public String getContent(){
         return content;
