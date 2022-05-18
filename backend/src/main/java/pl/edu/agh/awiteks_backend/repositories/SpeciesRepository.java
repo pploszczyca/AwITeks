21745c726695cf4607aadd8ac8e624d4b66a5b10
pploszczyca <piotrus.plo@gmail.com>
2022-05-18 23:30:18 +0200

    Repaired problem with adding specie

--- backend/src/main/java/pl/edu/agh/awiteks_backend/repositories/SpeciesRepository.java
+++ backend/src/main/java/pl/edu/agh/awiteks_backend/repositories/SpeciesRepository.java
@@ -1,11 +1,14 @@
 package pl.edu.agh.awiteks_backend.repositories;
 
+import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import pl.edu.agh.awiteks_backend.models.Species;
 
-import java.util.Optional;
-
 public interface SpeciesRepository extends CrudRepository<Species, Integer> {
+    @Query("SELECT s " +
+            "FROM Species AS s " +
+            "WHERE s.id = ?1 AND s.creatorId IN (?2, -1)")  // -1 means that spiecie is public for everyone
     Optional<Species> findByIdAndCreatorId(int id, int creatorId);
 
     Boolean existsByIdAndCreatorId(int id, int creatorId);
