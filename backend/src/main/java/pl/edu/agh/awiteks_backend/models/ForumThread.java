1939b24ed72089f29857d333a4586e3d2e6d11db
chleb <bszlacht@student.agh.edu.pl>
2022-05-23 14:28:18 +0200

    AWIT - 88 After merge

--- backend/src/main/java/pl/edu/agh/awiteks_backend/models/ForumThread.java
+++ backend/src/main/java/pl/edu/agh/awiteks_backend/models/ForumThread.java
@@ -6,16 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "forum_thread")
--- backend/src/main/java/pl/edu/agh/awiteks_backend/models/User.java
+++ backend/src/main/java/pl/edu/agh/awiteks_backend/models/User.java
@@ -3,15 +3,8 @@ package pl.edu.agh.awiteks_backend.models;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.LinkedList;
 import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
+
 import org.springframework.context.annotation.Lazy;
 
 @Entity
