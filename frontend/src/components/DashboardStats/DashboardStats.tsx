721a7761dc0439b599e125a7f8511e0050aeb304
pploszczyca <piotrus.plo@gmail.com>
2022-05-27 00:28:31 +0200

    [AWIT-144] Repaired frontend stats view

--- frontend/src/components/DashboardStats/DashboardStats.tsx
+++ frontend/src/components/DashboardStats/DashboardStats.tsx
@@ -35,7 +35,7 @@ const DashboardStats: React.FC<{}> = () => {
                 <StatsCard>
                     <p className="card-title">Liczba zadbanych roślin</p>
                     {plantsStats ? (
-                        <p className="stat">{plantsStats.neglectedPlants}</p>
+                        <p className="stat">{plantsStats.wellGroomedPlants}</p>
                     ) : (
                         <strong>{GENERIC_ERROR_MESSAGE}</strong>
                     )}
@@ -46,7 +46,7 @@ const DashboardStats: React.FC<{}> = () => {
                 <StatsCard>
                     <p className="card-title">Liczba zaniedbanych roślin</p>
                     {plantsStats ? (
-                        <p className="stat">{plantsStats.wellGroomedPlants}</p>
+                        <p className="stat">{plantsStats.neglectedPlants}</p>
                     ) : (
                         <strong>{GENERIC_ERROR_MESSAGE}</strong>
                     )}
