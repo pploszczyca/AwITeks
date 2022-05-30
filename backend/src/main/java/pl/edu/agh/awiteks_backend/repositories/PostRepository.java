ba98671eed99c9d2a959451b054859ee6e2b0d08
pploszczyca <piotrus.plo@gmail.com>
2022-05-30 17:09:08 +0200

    [AWIT-157] Removed methods from repositories

--- backend/src/main/java/pl/edu/agh/awiteks_backend/repositories/PostRepository.java
+++ backend/src/main/java/pl/edu/agh/awiteks_backend/repositories/PostRepository.java
@@ -1,14 +1,8 @@
 package pl.edu.agh.awiteks_backend.repositories;
 
-import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import pl.edu.agh.awiteks_backend.models.ForumPost;
 
 public interface PostRepository extends CrudRepository<ForumPost, Integer> {
-    Optional<ForumPost> findByIdAndUserId(int id, int userId);
-
-    Iterable<ForumPost> findAllByUserId(int userId);
-
-    Boolean existsByIdAndUserId(int id, int userId);
 }
 
