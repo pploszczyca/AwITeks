645ae8ca18809d83a50ebdcab2ef9db055049aa4
dmncp <damiancyper@gmail.com>
2022-06-04 12:31:23 +0200

    [AWIT-163] new regex for email

--- frontend/src/components/LoginAndRegistrationPage/validators.tsx
+++ frontend/src/components/LoginAndRegistrationPage/validators.tsx
@@ -20,7 +20,7 @@ export const validateConfirmPassword = (pass: any, value: any) => {
 };
 
 export const validateEmail = (email: string) => {
-    const regexEmail = /^(([^<>()[\]\\.,;:\-\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    const regexEmail = /^(([^<>()[\]\\.,-;:\s@"]+(\.[^<>()[\]\\.,-;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
     if(!email){
         return REQUIRED;
