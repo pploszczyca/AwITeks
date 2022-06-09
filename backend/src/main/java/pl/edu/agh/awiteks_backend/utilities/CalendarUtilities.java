2b557383c461dc0d5359e88b502061acd6f4c10b
pploszczyca <piotrus.plo@gmail.com>
2022-06-09 10:27:35 +0200

    [AWIT-153] Reapet events working Need to repair date, beacuse it's one day before actual date

--- backend/src/main/java/pl/edu/agh/awiteks_backend/utilities/CalendarUtilities.java
+++ backend/src/main/java/pl/edu/agh/awiteks_backend/utilities/CalendarUtilities.java
@@ -1,16 +1,16 @@
 package pl.edu.agh.awiteks_backend.utilities;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import net.fortuna.ical4j.model.Calendar;
 import net.fortuna.ical4j.model.Date;
-import net.fortuna.ical4j.model.Dur;
 import net.fortuna.ical4j.model.component.VEvent;
 import net.fortuna.ical4j.model.property.CalScale;
-import net.fortuna.ical4j.model.property.Duration;
 import net.fortuna.ical4j.model.property.ProdId;
+import net.fortuna.ical4j.model.property.RRule;
 import net.fortuna.ical4j.model.property.Version;
 import net.fortuna.ical4j.util.RandomUidGenerator;
 import net.fortuna.ical4j.util.UidGenerator;
@@ -22,8 +22,6 @@ import pl.edu.agh.awiteks_backend.models.User;
 public class CalendarUtilities {
 
     private static final String DATE_FORMAT = "yyyy-MM-dd";
-
-    private static final long DAY_TIME = 24 * 60 * 60 * 1000;
 
     private final SimpleDateFormat simpleDateFormat =
             new SimpleDateFormat(DATE_FORMAT);
@@ -37,7 +35,8 @@ public class CalendarUtilities {
         calendar.getProperties().add(Version.VERSION_2_0);
         calendar.getProperties().add(CalScale.GREGORIAN);
 
-        makePlantsVEvents(user.getUserPlants()).forEach(vEvent -> calendar.getComponents().add(vEvent));
+        makePlantsVEvents(user.getUserPlants()).forEach(
+                vEvent -> calendar.getComponents().add(vEvent));
 
         return calendar;
     }
@@ -45,32 +44,38 @@ public class CalendarUtilities {
     private List<VEvent> makePlantsVEvents(List<Plant> plants) {
         return plants.stream().flatMap(
                 plant -> plant.getPlantActivities().stream().map(activity -> {
-                    final var duration = getDuration(activity);
+                    final var event =
+                            new VEvent(stringToDate(activity.getDate()),
+                                    plant.getName() + ": " +
+                                            activity.getActivityType().name());
 
-                    final var event = new VEvent(stringToDate(activity.getDate()), duration,
-                            plant.getName() + ": " +
-                                    activity.getActivityType().name());
                     event.getProperties().add(uidGenerator.generateUid());
+                    try {
+                        event.getProperties().add(new RRule(
+                                makeRecurrencePattern(getDuration(activity))));
+                    } catch (ParseException e) {
+                        throw new RuntimeException(e);
+                    }
 
                     return event;
                 })).toList();
     }
 
-    private Dur getDuration(Activity activity) {
+    private int getDuration(Activity activity) {
         final var species = activity.getPlant().getSpecies();
-        final var startDate = stringToDate(activity.getDate());
-        final Date endDate = switch (activity.getActivityType()) {
-            case WATERING -> new Date(
-                    startDate.getTime() + species.getWaterRoutine() * DAY_TIME);
-            case FERTILISATION -> new Date(startDate.getTime() +
-                    species.getFertilizationRoutine() * DAY_TIME);
-        };
 
-        return new Duration(startDate, endDate).getDuration();
+        return switch (activity.getActivityType()) {
+            case WATERING -> species.getWaterRoutine();
+            case FERTILISATION -> species.getFertilizationRoutine();
+        };
     }
 
     @SneakyThrows
     private Date stringToDate(String dateString) {
         return new Date(simpleDateFormat.parse(dateString));
+    }
+
+    private String makeRecurrencePattern(int days) {
+        return "FREQ=DAILY;INTERVAL=" + days;
     }
 }
