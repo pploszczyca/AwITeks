ff97c9e75d06935d96e54917d311f45271fbf26c
kkoz34 <jkozlak@student.agh.edu.pl>
2022-06-10 17:59:50 +0200

    [AWIT-164] CR update

--- backend/src/main/java/pl/edu/agh/awiteks_backend/services/UserService.java
+++ backend/src/main/java/pl/edu/agh/awiteks_backend/services/UserService.java
@@ -1,25 +1,23 @@
 package pl.edu.agh.awiteks_backend.services;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import pl.edu.agh.awiteks_backend.api.users.body_models.UserForumThreadsSummary;
 import pl.edu.agh.awiteks_backend.api.users.body_models.UserInfo;
 import pl.edu.agh.awiteks_backend.api.users.body_models.UserMainSummary;
+import pl.edu.agh.awiteks_backend.api.users.body_models.UserPlantSummary;
 import pl.edu.agh.awiteks_backend.mappers.UserMapper;
-import pl.edu.agh.awiteks_backend.models.ActivityType;
-import pl.edu.agh.awiteks_backend.models.ForumPost;
 import pl.edu.agh.awiteks_backend.models.User;
 import pl.edu.agh.awiteks_backend.repositories.UserRepository;
 import pl.edu.agh.awiteks_backend.utilities.CalendarUtilities;
 import pl.edu.agh.awiteks_backend.utilities.ForumUtilities;
 import pl.edu.agh.awiteks_backend.utilities.PlantUtilities;
 import java.util.List;
-
 
 @Service
 @RequiredArgsConstructor
@@ -66,18 +64,18 @@ public class UserService {
     public UserMainSummary getUserMainSummary(int userId) {
         final User user = userRepository.findById(userId).orElseThrow();
 
-        final List<Pair<String, List<ActivityType>>> missedActivities = plantUtilities.getNeglectedPlants(user).stream()
+        final List<UserPlantSummary> missedActivities = plantUtilities.getNeglectedPlants(user).stream()
                 .map((pair) ->
-                        Pair.of(pair.getLeft().getName(), pair.getRight())
+                       new UserPlantSummary(pair.getLeft().getName(), pair.getRight())
                 )
                 .limit(NUM_OF_MAIN_SUMMARY).toList();
 
-        final List<Pair<String, List<ActivityType>>> todayActivities =
+        final List<UserPlantSummary> todayActivities =
                 plantUtilities.findAllPlantsThatNeedActivitiesToday(user).map((pair) ->
-                        Pair.of(pair.getLeft().getName(), pair.getRight())
+                        new UserPlantSummary(pair.getLeft().getName(), pair.getRight())
                 ).limit(NUM_OF_MAIN_SUMMARY).toList();
 
-        final List<Pair<String, ForumPost>> forumThreads = forumUtilities.getNewestForumPostsFromFollowedThreads(user);
+        final List<UserForumThreadsSummary> forumThreads = forumUtilities.getNewestForumPostsFromFollowedThreads(user);
         return new UserMainSummary(missedActivities, todayActivities, forumThreads);
     }
 }
--- backend/src/main/java/pl/edu/agh/awiteks_backend/utilities/ForumUtilities.java
+++ backend/src/main/java/pl/edu/agh/awiteks_backend/utilities/ForumUtilities.java
@@ -1,6 +1,6 @@
 package pl.edu.agh.awiteks_backend.utilities;
 
-import org.apache.commons.lang3.tuple.Pair;
+import pl.edu.agh.awiteks_backend.api.users.body_models.UserForumThreadsSummary;
 import pl.edu.agh.awiteks_backend.models.ForumPost;
 import pl.edu.agh.awiteks_backend.models.User;
 import java.util.Comparator;
@@ -9,10 +9,12 @@ import java.util.List;
 public class ForumUtilities {
     private static final int LIMIT = 5;
 
-    public List<Pair<String, ForumPost>> getNewestForumPostsFromFollowedThreads(User user) {
+    public List<UserForumThreadsSummary> getNewestForumPostsFromFollowedThreads(User user) {
         return user.getFollowedThreads().stream().limit(LIMIT).map(forumThread -> {
             forumThread.getForumPosts().sort(Comparator.comparing(ForumPost::getDate));
-            return Pair.of(forumThread.getTitle(), forumThread.getForumPosts().stream().findFirst().orElseThrow());
+            return new UserForumThreadsSummary(forumThread.getId(), forumThread.getTitle(),
+                    forumThread.getForumPosts().stream().findFirst().orElseThrow().getContent(),
+                   forumThread.getForumPosts().stream().findFirst().orElseThrow().getDate().toString());
         }).toList();
     }
 }
