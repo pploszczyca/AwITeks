e4a3748cf97ef11ec70f62aa48065fb95da98250
dmncp <damiancyper@gmail.com>
2022-06-10 19:37:53 +0200

    [AWIT-166] stats list in progress

--- frontend/src/components/DashboardInfo/DashboardInfo.tsx
+++ frontend/src/components/DashboardInfo/DashboardInfo.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import {Col} from "react-bootstrap";
 import {InfoElement, MobileRow} from "./DashboardInfoStyle";
+import {useQuery} from "react-query";
+import {getApis} from "../../api/initializeApis";
+import {errorMsg} from "../../utils/constants";
 
 
 const DashboardInfo: React.FC<{}> = () => {
+    let { data: summaryLists, isLoading: summaryListsLoading } =
+        useQuery(
+            ['users', 'userMainSummary'],
+            () => getApis().userApi..then(resp => resp.data),
+            {onError: (error) => errorMsg()}
+        );
+
     return (
         <MobileRow className="d-flex justify-content-center mt-3">
             <Col xxl={4} sm={6} xs={12} className="mt-2">
---
+++ frontend/src/components/DashboardInfo/StatsList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function StatsList(props) {
+    const stats = props.data;
+
+    return (
+        <div></div>
+    );
+}
+
+export default StatsList;
