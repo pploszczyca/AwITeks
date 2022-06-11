df3dd07250515eaf6d8b395a7bb9c4dfb653aaed
pploszczyca <piotrus.plo@gmail.com>
2022-06-11 19:14:07 +0200

    [AWIT-166] Changed sort to max in getNewestForumPostsFromFollowedThreads

--- backend/src/main/java/pl/edu/agh/awiteks_backend/utilities/ForumUtilities.java
+++ backend/src/main/java/pl/edu/agh/awiteks_backend/utilities/ForumUtilities.java
@@ -2,6 +2,7 @@ package pl.edu.agh.awiteks_backend.utilities;
 
 import pl.edu.agh.awiteks_backend.api.users.body_models.UserForumThreadsSummary;
 import pl.edu.agh.awiteks_backend.models.ForumPost;
+import pl.edu.agh.awiteks_backend.models.ForumThread;
 import pl.edu.agh.awiteks_backend.models.User;
 import java.util.Comparator;
 import java.util.List;
@@ -11,11 +12,18 @@ public class ForumUtilities {
 
     public List<UserForumThreadsSummary> getNewestForumPostsFromFollowedThreads(User user) {
         return user.getFollowedThreads().stream().limit(LIMIT).map(forumThread -> {
-            forumThread.getForumPosts().sort(Comparator.comparing(ForumPost::getDate));
-            final var forumPost = forumThread.getForumPosts().stream().reduce((first, second) -> second).orElseThrow();
+            final var forumPost = getNewestForumPost(forumThread);
             return new UserForumThreadsSummary(forumThread.getId(), forumThread.getTitle(),
                     forumPost.getContent(),
                    forumPost.getDate().toString());
         }).toList();
+    }
+
+    private ForumPost getNewestForumPost(ForumThread forumThread) {
+        return forumThread
+                .getForumPosts()
+                .stream()
+                .max(Comparator.comparing(ForumPost::getDate))
+                .orElseThrow();
     }
 }
